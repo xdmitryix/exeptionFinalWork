698721e48914aee2ef13aa5d27b3dbd55c5080b4
dmitry <rbdeman@yandex.ru>
2023-08-14 20:31:32 +0300

    Добавил методы для исключений количества введенных данных

---
+++ LessMyCountExaption.java
@@ -0,0 +1,6 @@
+public class LessMyCountExaption extends Exception{
+    
+    public LessMyCountExaption(){
+        super("Количество введенных данных меньше 6! Попробуйте ещё раз.");
+    }
+}
---
+++ MoreMyCountExaption.java
@@ -0,0 +1,6 @@
+public class MoreMyCountExaption extends Exception{
+
+    public MoreMyCountExaption(){
+        super("Количество введенных данных больше 6! Попробуйте ещё раз.");
+    }
+}
